5f731245fed06e91bc434f0fd727251ca5817b70
MattosCode12 <enzo_mattos@estudante.sesisenai.org.br>
2026-04-29 09:03:50 -0300

    ADD - Fórum de discussão e mudanças nos layouts

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function login() {
         erro.innerText = "Usuário ou senha incorretos!";
     }
 }
+window.onload = function() {
+    mostrarComentarios();
+}
+
+function adicionarComentario() {
+    let nome = document.getElementById("nome").value;
+    let texto = document.getElementById("comentario").value;
+
+    if (nome === "" || texto === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    let comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
+
+    comentarios.push({
+        nome: nome,
+        texto: texto
+    });
+
+    localStorage.setItem("comentarios", JSON.stringify(comentarios));
+
+    document.getElementById("nome").value = "";
+    document.getElementById("comentario").value = "";
+
+    mostrarComentarios();
+}
+
+function mostrarComentarios() {
+    let lista = document.getElementById("listaComentarios");
+    lista.innerHTML = "";
+
+    let comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
+
+    comentarios.forEach(c => {
+        lista.innerHTML += `
+            <div class="card p-3 mb-2">
+                <strong>${c.nome}:</strong>
+                <p>${c.texto}</p>
+            </div>
+        `;
+    });
+}
